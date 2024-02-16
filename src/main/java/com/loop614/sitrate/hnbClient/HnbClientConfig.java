bbafee27058e79d97ebfce6ba608f118cd11c8a3
loop <tony.kusic@gmail.com>
2024-02-16 10:30:57 -0500

    removed hnbclient file cache, using static hashmap instead

--- src/main/java/com/loop614/sitrate/hnbClient/HnbClientConfig.java
+++ src/main/java/com/loop614/sitrate/hnbClient/HnbClientConfig.java
@@ -1,7 +1,6 @@
 package com.loop614.sitrate.hnbClient;
 
 public class HnbClientConfig {
-    public static String CACHEDIRECTORY = "/src/main/java/com/loop614/sitrate/hnbClient/cache/";
     public static String BASEURL = "https://api.hnb.hr";
     public static String URL = "/tecajn-eur/v3";
 }
--- src/main/java/com/loop614/sitrate/hnbClient/domain/HnbFetcherImpl.java
+++ src/main/java/com/loop614/sitrate/hnbClient/domain/HnbFetcherImpl.java
@@ -1,20 +1,14 @@
 package com.loop614.sitrate.hnbClient.domain;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.json.JSONTokener;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.loop614.sitrate.hnbClient.HnbClientConfig;
 import com.loop614.sitrate.hnbClient.exception.HnbClientException;
 import com.loop614.sitrate.hnbClient.transfer.HnbCurrency;
@@ -22,56 +16,21 @@ import com.loop614.sitrate.hnbClient.transfer.HnbCurrencyReceived;
 
 @Service
 public class HnbFetcherImpl implements HnbFetcher {
+    private static HashMap<String, List<HnbCurrency>> cacheHnbCurrency = new HashMap<String, List<HnbCurrency>>();;
+
     public List<HnbCurrency> currencyEur() throws HnbClientException {
         LocalDate localDate = LocalDate.now();
         String todayString = localDate.toString();
-        String cacheDirectoryPath = System.getProperty("user.dir") + HnbClientConfig.CACHEDIRECTORY;
-        File cacheDirectory = new File(cacheDirectoryPath);
-        if (!cacheDirectory.isDirectory()) {
-            cacheDirectory.mkdirs();
+        List<HnbCurrency> cachedList = HnbFetcherImpl.cacheHnbCurrency.get(todayString);
+        if(cachedList != null) {
+            return cachedList;
         }
-        String cachePath = cacheDirectoryPath + HnbClientConfig.URL.replace("/", "") + "-" + todayString + ".json";
-        File f = new File(cachePath);
-        if(f.exists() && !f.isDirectory()) {
-            return getHnbCurrencyFromCache(cachePath);
-        }
+
         List<HnbCurrency> hnbCurrencies = this.getHnbCurrency();
-        this.cleanCacheDirectory(cacheDirectory);
-        this.saveHnbCurrencyCache(hnbCurrencies, cachePath);
+        HnbFetcherImpl.cacheHnbCurrency = new HashMap<String, List<HnbCurrency>>();
+        HnbFetcherImpl.cacheHnbCurrency.put(todayString, hnbCurrencies);
 
         return hnbCurrencies;
-    }
-
-    private void saveHnbCurrencyCache(List<HnbCurrency> hnbCurrencies, String cachePath) {
-        try {
-            new ObjectMapper().writeValue(new File(cachePath), hnbCurrencies);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void cleanCacheDirectory(File cacheDirectory) {
-        for(File file: cacheDirectory.listFiles()) {
-            if (!file.isDirectory()) {
-                file.delete();
-            }
-        }
-    }
-
-    private List<HnbCurrency> getHnbCurrencyFromCache(String cachePath) {
-        try (FileReader reader = new FileReader(cachePath)) {
-            JSONArray jsonArray = new JSONArray(new JSONTokener(reader));
-            ArrayList<HnbCurrency> currencyList = new ArrayList<>();
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject jsonCurrency = jsonArray.getJSONObject(i);
-                HnbCurrency currency = HnbCurrency.fromJson(jsonCurrency);
-                currencyList.add(currency);
-            }
-            return currencyList;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return new ArrayList<>();
-        }
     }
 
     private List<HnbCurrency> getHnbCurrency() {
